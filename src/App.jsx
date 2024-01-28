142448ea3bdbc021e4ce909d8252e4cd61b49f32
SubhaChandiran <jerrysubash@gmail.com>
2024-01-28 15:26:49 +0530

    register + login screen frontend

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import NavBar from "./components/NavBar";
 import { BrowserRouter, Route, Lik, Routes } from "react-router-dom";
 import Homescreen from "./screens/Homescreen";
 import Bookingscreen from "./screens/Bookingscreen";
+import Registerscreen from "./screens/Registerscreen";
+import Loginscreen from "./screens/Loginscreen";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/home" exact Component={Homescreen} />
           <Route path="/book/:roomid" exact Component={Bookingscreen} />
+          <Route path="/register" exact Component={Registerscreen} />
+          <Route path="/login" exact Component={Loginscreen} />
         </Routes>
       </BrowserRouter>
     </div>
