de56a893feeca2f604ef22dbb87ff87b186c000b
SubhaChandiran <jerrysubash@gmail.com>
2024-01-28 13:00:26 +0530

    #13 booking room screen frontend

--- src/screens/Bookingscreen.jsx
+++ src/screens/Bookingscreen.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 function Bookingscreen({ match }) {
   const { roomid } = useParams();
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
   const [room, setRoom] = useState();
 
@@ -28,8 +28,48 @@ function Bookingscreen({ match }) {
 
   return (
     <>
-      <div>Bookingscreen</div>
-      <h1>Room id = {roomid}</h1>
+      <div className="m-5">
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <h1>Error...</h1>
+        ) : (
+          <div>
+            <div className="row justify-content-center mt-5 bs">
+              <div className="col-md-6">
+                <h1>{room.name}</h1>
+                <img src={room.imageurls[0]} className="bigimg" />
+              </div>
+              <div className="col-md-6">
+                {/* --- Booking Details --- */}
+                <div style={{ textAlign: "right" }}>
+                  <h1>Booking Details</h1>
+                  <hr />
+                  <b>
+                    <p>Name : </p>
+                    <p>From Data : </p>
+                    <p>To Date : </p>
+                    <p>Max Count : {room.maxcount} </p>
+                  </b>
+                </div>
+                {/* ---- Amount ---- */}
+                <div style={{ textAlign: "right" }}>
+                  <b>
+                    <h1>Amount</h1>
+                    <hr />
+                    <p>Total Days : </p>
+                    <p>Rent Per Day : {room.rentperday} </p>
+                    <h1>Total Amount : </h1>
+                  </b>
+                </div>
+                <div style={{ float: "right" }}>
+                  <button className="btn btn-primary">Pay Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
